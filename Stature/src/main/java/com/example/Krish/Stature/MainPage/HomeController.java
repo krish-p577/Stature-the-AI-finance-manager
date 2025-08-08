bc7794400c1af6604d6f352cb881cff4dafdd90c
Krish Patel <krishpatel8976@gmail.com>
2025-08-08 16:40:34 -0400

    basic form functionality

--- Stature/src/main/java/com/example/Krish/Stature/MainPage/HomeController.java
+++ Stature/src/main/java/com/example/Krish/Stature/MainPage/HomeController.java
@@ -4,37 +4,45 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.example.Krish.Stature.MainPage.Model.User;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 
 @Controller
 @SessionAttributes("User")
-@RequestMapping
+@RequestMapping()
 public class HomeController {
 
     @Value("${spring.application.name}")
     private String appName;
  
-    @GetMapping
-    public String get(Model model){
-        model.addAttribute("message", "Enter your current income here:");
+    @GetMapping("home")
+    public String Home(Model model){
         model.addAttribute("user", new User());
-        return "index.html";
+        model.addAttribute("name", "Enter your yearly income here:");
+        // model.addAttribute("income", "fake values");
+        model.addAttribute("expences", "Enter your monthly expences here, click the plus to add more");
+        return "home.html";
     }
 
-    @PostMapping("")
-    public String postMethodName(@RequestBody String entity) {
-        //TODO: process POST request
-        
-        return entity;
+    @PostMapping("home")
+    public String Home(@ModelAttribute User user, Model model){
+
+        System.out.println("got info" + user.getIncome());
+        model.addAttribute("income", "you make " + user.getIncome() + " a year");
+        model.addAttribute("expences","You lose " +  user.getExpences() + " a month");       
+
+        return "home.html";
     }
     
-
+    @GetMapping("option")
+    public String option(Model model) {
+        return new String();
+    }
     
 }
 
